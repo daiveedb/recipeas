328780358ad6eff82190c489a1c84073f2e40f4b
David Bajomo <bajomodavid18@gmail.com>
2023-08-07 21:38:31 +0100

    category data added

---
+++ src/recipeasData.js
@@ -0,0 +1,96 @@
+
+
+
+
+const categories = [
+    {
+        categoryId:1,
+        categoryName:"Vegan",
+        categoryImageUrl:"",
+        categoryCallUrl:"",
+    },
+    {
+        categoryId:2,
+        categoryName:"African",
+        categoryImageUrl:"",
+        categoryCallUrl:"",
+    },
+    {
+        categoryId:3,
+        categoryName:"Chinese",
+        categoryImageUrl:"",
+        categoryCallUrl:"",
+    },
+    {
+        categoryId:4,
+        categoryName:"Italian",
+        categoryImageUrl:"",
+        categoryCallUrl:"",
+    },
+    {
+        categoryId:5,
+        categoryName:"Desert",
+        categoryImageUrl:"",
+        categoryCallUrl:"",
+    },
+    {
+        categoryId:6,
+        categoryName:"Main Course",
+        categoryImageUrl:"",
+        categoryCallUrl:"",
+    },
+    {
+        categoryId:7,
+        categoryName:"Appetizers",
+        categoryImageUrl:"",
+        categoryCallUrl:"",
+    },
+    {
+        categoryId:8,
+        categoryName:"Soups",
+        categoryImageUrl:"",
+        categoryCallUrl:"",
+    },
+    {
+        categoryId:9,
+        categoryName:"Salad",
+        categoryImageUrl:"",
+        categoryCallUrl:"",
+    },
+    {
+        categoryId:10,
+        categoryName:"Lactose Intolerant",
+        categoryImageUrl:"",
+        categoryCallUrl:"",
+    },
+    {
+        categoryId:11,
+        categoryName:"Bread & pastries",
+        categoryImageUrl:"",
+        categoryCallUrl:"",
+    },
+    {
+        categoryId:12,
+        categoryName:"Side Meals",
+        categoryImageUrl:"",
+        categoryCallUrl:"",
+    },
+    {
+        categoryId:13,
+        categoryName:"Breakfast",
+        categoryImageUrl:"",
+        categoryCallUrl:"",
+    },
+    {
+        categoryId:14,
+        categoryName:"Snacks",
+        categoryImageUrl:"",
+        categoryCallUrl:"",
+    },
+    {
+        categoryId:15,
+        categoryName:"Drinks",
+        categoryImageUrl:"",
+        categoryCallUrl:"",
+    },
+]
