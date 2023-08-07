bdd3b37e14b2af6014868d8b38fafe3571dffde6
David Bajomo <bajomodavid18@gmail.com>
2023-08-07 12:42:52 +0100

    mobile navbar implemented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,18 +5,18 @@ const Navbar = () => {
   const navigate = useNavigate()
   return (
     <div className='text-center py-10'>
-        <h1 className='text-6xl font-pacifico font-bold italic pb-10'>ReciPeas</h1>
-        <div className='w-[40%] flex justify-evenly mx-auto items-center pt-5'>
-            <div className='font-light cursor-pointer tracking-tighter' onClick={() => navigate('/')}>
+        <h1 className='text-4xl md:text-5xl lg:text-6xl font-pacifico font-bold italic pb-5 md:pb-10'>ReciPeas</h1>
+        <div className='w-[80%] md:w-[60%] lg:w-[40%] flex justify-evenly mx-auto items-center pt-2 md:pt-5'>
+            <div className='text-sm sm:text-base md:font-light cursor-pointer tracking-tighter' onClick={() => navigate('/')}>
               <p>Home</p>
             </div>
-            <div className='font-light cursor-pointer tracking-tighter' onClick={() => navigate('/categories')}>
+            <div className='text-sm sm:text-base md:font-light cursor-pointer tracking-tighter' onClick={() => navigate('/categories')}>
               <p>Categories</p>
             </div>
-            <div className='font-light cursor-pointer tracking-tighter' onClick={() => navigate('/')}>
+            <div className='text-sm sm:text-base md:font-light cursor-pointer tracking-tighter' onClick={() => navigate('/')}>
               <p>Search</p>
             </div>
-            <div className='font-light cursor-pointer tracking-tighter' onClick={() => navigate('/')}>
+            <div className='text-sm sm:text-base md:font-light cursor-pointer tracking-tighter' onClick={() => navigate('/')}>
               <p>Surprise</p>
             </div>
         </div>
