06ceccaead2f1916e658525a4def8ddd85a3c8e6
David Bajomo <bajomodavid18@gmail.com>
2023-08-13 13:15:23 +0100

    categories snippet optimezed and finishing touches

--- src/components/CategoriesSnippet.jsx
+++ src/components/CategoriesSnippet.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import {categories} from "../recipeasData"
 import CatetegorySmallIcon from './CatetegorySmallIcon'
+import {AiOutlineRight} from "react-icons/ai"
+import { useNavigate } from 'react-router-dom'
 
 const CategoriesSnippet = () => {
     const size = 6
     const categoriesSnippet = categories.slice(0,size)
-
+    const navigate = useNavigate()
   return (
-    <div className='w-full py-10'>
-        <h2 className='text-2xl text-center sm:text-left sm:text-3xl font-poppins font-semibold p-10'>Popular Categories</h2>
+    <div className='w-full py-10 categories-snippet'>
+        <h2 className='text-2xl text-center sm:text-left sm:text-3xl md:text-4xl font-poppins font-semibold tracking-tighter p-10 xl:px-20'>Popular Categories</h2>
         <div className='grid grid-cols-3 justify-items-center lg:grid-cols-6 sm:px-10 xl:px-20'>
             {
                 categoriesSnippet.map((category) => {
                     return <CatetegorySmallIcon key={category.id} category={category}/>
                 })
             }
+        </div>
+        <div className='pb-7 px-20 see-more-div text-dimOrange'>
+            <div onClick={() => navigate('/categories')} className='mx-auto w-[150px] py-3 border border-transparent hover:border-brightOrange rounded-lg cursor-pointer flex items-center justify-center transition-all'>
+                <p className='mr-3'>See More</p> 
+                <span><AiOutlineRight/></span>
+            </div>
         </div>
     </div>
   )
--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import PageHeader from '../components/PageHeader'
 import CategoriesGrid from '../components/CategoriesGrid'
 import Footer from '../components/Footer'
 
 const Categories = () => {
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  })
+
   return (
     <div>
       <PageHeader header = 'Recipeas Categories'/>
