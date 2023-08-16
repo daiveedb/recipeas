3cc513b1b5e71538fb7b20a32e4fb3ec67e434f2
David Bajomo <bajomodavid18@gmail.com>
2023-08-16 21:28:15 +0100

    newsletter made responsive

--- src/components/homeComponents/NewsLetter.jsx
+++ src/components/homeComponents/NewsLetter.jsx
@@ -12,20 +12,21 @@ const NewsLetter = () => {
     const handleSubmit = (event) => {
         event.preventDefault()
         alert("Email Successfully Submitted, Thank you")
+        setEmail('')
     }
 
   return (
-    <div className='w-full bg-lightBlue py-32 px-40 text-center'>
+    <div className='w-full bg-lightBlue py-32 px-10 sm:px-20 md:px-40 text-center'>
         <div className='pb-10'>
-            <h1 className='text-7xl font-pacifico font-bold capitalize pb-6'>
+            <h1 className='text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-pacifico font-bold capitalize pb-6'>
                 Deliciousness in your inbox
             </h1>
-            <p className='text-lg tracking-wide font-sans font-light capitalize'>enjoy weekly handpicked recipes and recommendations</p>
+            <p className='text-sm md:text-base lg:text-lg tracking-wide font-sans font-light capitalize'>enjoy weekly handpicked recipes and recommendations</p>
         </div>
         <div className='flex justify-center items-center'>
-            <form onSubmit={() => handleSubmit()}>
+            <form onSubmit={(e) => handleSubmit(e)}>
                 <input
-                className ='bg-white outline-none rounded-l-md p-3 w-[300px] border border-r-0 border-transparent hover:border-gray-400'
+                className ='bg-white outline-none rounded-l-md p-3 w-[180px] sm:w-[200px] md:w-[300px] border border-r-0 border-transparent hover:border-gray-400'
                 placeholder = 'Enter Your Email'
                 onChange = {(e) => handleEmail(e.target.value)}
                 value={email}
