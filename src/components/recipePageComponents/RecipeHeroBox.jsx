4e1e751f4bab9c41ff77a12d94943271ddc9545a
David Bajomo <bajomodavid18@gmail.com>
2023-08-28 19:00:45 +0100

    ingredients updated

--- src/components/recipePageComponents/RecipeHeroBox.jsx
+++ src/components/recipePageComponents/RecipeHeroBox.jsx
@@ -1,33 +1,44 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import IngredientItem from './IngredientItem'
 
 const RecipeHeroBox = ({state}) => {
 
 
 
-    const ingridients = [state.usedIngredients,state.missedIngredients].flat() 
+   const [ingredients, setIngredients] = useState([])
+
+   const getIngredients = () => {
+    let ingredients = []
+
+    for (let i in state.nutrition.ingredients){
+        const notIncluded = ingredients.find((item) => item.id === state.nutrition.ingredients[i].id) === undefined
+        if(notIncluded){
+            ingredients.push(state.nutrition.ingredients[i])
+        }
+    }
+    setIngredients(ingredients);
+   }
+
+   const getEquipment = () => {
+    let equipment = []
+   } 
 
     useEffect(()=> {
-        // console.log(ingridients);
+      getIngredients()
+      getEquipment()
+      console.log(state);
     },[])
 
 
   return (
-    <div className='w-full p-5 flex gap-x-5 justify-between items-start bg-darkGray h-max'>
-        <div className='flex-1 '>
-            <p className='text-2xl font-bold pb-3'>Ingridients</p>
-           {ingridients.map((ingridient)=> {
-                return(
-                <div key={ingridient.id}>
-                    <p className='text-black'>{`${ingridient.amount} ${ingridient.unit} ${ingridient.originalName}`}</p>
-                </div>
-            )})}
+    <div className='w-[90vw] xl:w-[80vw] mx-auto p-5 my-5 flex justify-between items-center h-[650px]'>
+        <div className='flex-1 bg-lightGray p-16 rounded h-full overflow-y-scroll'>
+            <p className='text-2xl font-black pb-3 uppercase text-darkerGray'>Ingridients</p>
+           {ingredients.map((ingridient)=> {
+                return <IngredientItem key={ingridient.id} ingridient={ingridient}/>})}
         </div>
-        <div className='flex-1'>
-            <p className='text-2xl font-bold pb-3'>Equipment</p>
-
-        </div>
-        <div className='flex-1'>
-
+        <div className='flex-1 bg-darkGray w-full h-[90%] -translate-x-5 p-16'>
+        <p className='text-2xl font-black pb-3 uppercase text-lightGray'>Equipment</p>
         </div>
     </div>
   )
